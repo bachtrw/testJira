1f20ea42cf28d97f87106366b0cc19f3638bda0d
Bachtiar Widagdo <bachtiarwidagdo@Bachtiars-MacBook-Pro.local>
2023-01-23 11:14:59 +0700

    Create variable inside index.js

---
+++ git-commands.js
@@ -0,0 +1,6 @@
+//1. git init (inside project directory) // ini untuk pertama kali menambahkan di git folder/reponya
+//2. menambahkan di git (git init) atau cara lainnya (git add .)
+//3. git commit -m "Create variable inside index.js"
+//4. git log
+//5. git remote add origin https://github.com/bachtrw/testJira.git // ini hanya pertama kali saat membuat repo baru di git
+//6. git push origin (nama branch ex main/master) // ini untuk push file ke origin master
